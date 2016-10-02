02dfc0c313c035be66890ad222a35a858e3ed7e4
Ivaylo Goranov <ivaylo.v.goranov@gmail.com>
2016-10-02 14:30:44 +0300

    updates

--- DB-Basics-MSSQL-Data-Aggregation-Exercises/DB-Basics-MSSQL-Data-Aggregation-Exercises.sql
+++ DB-Basics-MSSQL-Data-Aggregation-Exercises/DB-Basics-MSSQL-Data-Aggregation-Exercises.sql
@@ -35,3 +35,12 @@ FROM Employees
 GROUP BY DepartmentID
 HAVING MAX(Salary) BETWEEN 30000 AND 70000
 ---------------------------------------------------------------------
+/*Third Highest Salary*/
+
+SELECT DepartmentID, 
+	(SELECT DISTINCT Salary FROM Employees WHERE DepartmentID = e.DepartmentID 
+	 ORDER BY Salary DESC OFFSET 2 ROWS FETCH NEXT 1 ROWS ONLY)AS ThirdHighestSalary
+FROM Employees e
+WHERE (SELECT DISTINCT Salary FROM Employees WHERE DepartmentID = e.DepartmentID 
+       ORDER BY Salary DESC OFFSET 2 ROWS FETCH NEXT 1 ROWS ONLY) IS NOT NULL
+GROUP BY DepartmentID
