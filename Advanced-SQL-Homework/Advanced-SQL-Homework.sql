7b603ac665167ad91f8502bdd4fa51b56b420ffb
Ivaylo Goranov <ivaylo.v.goranov@gmail.com>
2016-10-04 23:44:22 +0300

    updates

--- Advanced-SQL-Homework/Advanced-SQL-Homework.sql
+++ Advanced-SQL-Homework/Advanced-SQL-Homework.sql
@@ -177,3 +177,14 @@ INNER JOIN Towns t
 ON a.TownID = t.TownID
 GROUP BY t.Name
 ---------------------------------------------------------------------
+SELECT CountryName, 
+	   CountryCode,
+	   CASE
+			WHEN CurrencyCode <> 'EUR' THEN 'Not Euro'
+			WHEN CurrencyCode IS NULL THEN 'Not Euro'
+			ELSE 'Euro'
+	   END
+	   AS Currency		
+FROM Countries
+ORDER BY CountryName
+---------------------------------------------------------------------
